6bcf99b03824d571b2a344a4ae72f690df978dc1
Eduardo Gomez <eduman.000@gmail.com>
2022-09-13 09:00:04 -0300

    Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { Hero } from "./components/Hero";
-import  Navbar  from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import { About } from "./components/About";
-import {Concesionario} from './components/Concesionario'
+import { Concesionario } from './components/Concesionario'
 import { Footer } from "./components/Footer";
 import Maquinas from "./components/Maquinas";
 import Contact from "./components/Contact";
+import { Login } from "./components/Login";
 function App() {
   return (
-  <>
-  <Navbar />
-  <Hero /> 
-  <About />
-  <Concesionario />
-  <Maquinas />
-  <Contact />
-  <Footer />
+    <>
+      {/* <Navbar />
+      <Hero />
+      <About />
+      <Concesionario />
+      <Maquinas />
+      <Contact />
+      <Footer />
+      <Login /> */}
 
-  
-  </>
-  
+    </>
+
   )
 }
 
