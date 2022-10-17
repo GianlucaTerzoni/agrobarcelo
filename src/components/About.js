3c87e9105faf30a04d7cb08a8f9fd04ae9e20d23
GianlucaTerzoni <gianlucaterzoni@hotmail.com>
2022-10-17 16:54:53 -0300

    deleted buttoms

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@
             <h2><span>Sobre Nosotros</span></h2>
             <p>Somos una empresa dedicada a la venta de maquinarias agrícolas nuevas y usadas, repuestos y lubricantes específicos. Cubrimos las necesidades de todos nuestros clientes los 365 días del año, con inteligencia y empeño, sabemos lo que hacemos porque estamos en el rubro hace más de 30 años, logramos todo con mucho esfuerzo y dedicación. Pero todo no sería así sin nuestros fieles clientes, que siempre vuelven.</p>
             <div>
-                <button>Ver más</button>
+                
             </div>
         </div>
         </div>
--- src/components/Concesionario.js
+++ src/components/Concesionario.js
@@ -8,7 +8,7 @@ export const Concesionario = () => {
           <div className='content'>
             <h2><span>Somos Concesionarios directos de C.E.L.E.  S.R.L.</span></h2>
             <p>Representamos a la marca C.E.L.E. desde hace 30 años en la provincia de Buenos Aires, esta prestigiosa empresa de maquinarias agrícolas confía en nosotros por nuestro excelente trabajo y responsabilidad. Vendemos sus máquinas y productos, brindamos sus servicios de mantenimiento en pos venta y asesoramiento para un mejor rendimiento.</p>
-            <div><button>VER MÁS</button></div>
+            
         </div>
       </div> 
 
